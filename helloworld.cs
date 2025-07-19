a6e5528c2cdbd438d97028f04bbb47875c7d27d7
charan1018 <kcharan1018@gmail.com>
2025-07-19 12:20:50 +0530

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Hello
+{
+
+    static void Main(string[] args)
+    {
+
+        var i = 10;
+        var name = "charan";
+
+        Console.WriteLine(i + " " + name);
+        
+    }
+}
